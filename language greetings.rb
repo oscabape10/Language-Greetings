7c8ebac7746734db6df00d5bd991918954281517
Pachi10 <45542113+oscabape10@users.noreply.github.com>
2019-02-10 08:52:24 +0000

    Upload Languages code

---
+++ language greetings.rb
@@ -0,0 +1,12 @@
+puts "Hello there!"
+
+greeting = gets.chomp
+
+# Add your case statement below!
+case greeting
+  when "English" then puts "Hello!"
+  when "French" then puts "Bonjour!"
+  when "German" then puts "Guten Tag!"
+  when "Finnish" then puts "Haloo!"
+  else puts "Do not understan your language!"
+  end
